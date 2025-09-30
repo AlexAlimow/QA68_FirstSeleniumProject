f1b10aef174c32e47ab2b1a7efb2e454a84dc549
AlexAlimow <gorlum007rus@gmail.com>
2025-09-30 17:51:35 +0200

    HW_QA_30.09.2025

---
+++ src/test/java/com/ait/tests/FindElementsInDemoWebShop.java
@@ -0,0 +1,49 @@
+package com.ait.tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class FindElementsInDemoWebShop {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.get("https://demowebshop.tricentis.com/");
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void findElementsOnPage() {
+        WebElement header = driver.findElement(By.className("header")); // Нашли класс header
+        System.out.println("search by header class: \n" + header.getText());
+
+        List<WebElement> searchBox = driver.findElements(By.ByClassName.className("search-box")); // Нашли класс search-box
+        System.out.println("search by search.box: " + searchBox.size());
+
+        WebElement logo = driver.findElement(By.cssSelector("div.header-logo img"));
+        System.out.println("Logo src: " + logo.getAttribute("src")); // Проверяем логотип
+
+        List<WebElement> links = driver.findElements(By.tagName("a"));
+        System.out.println("Total links: " + links.size()); // Получаем количество ссылок
+
+        driver.findElement(By.linkText("Books")).click();
+        WebElement h1 = driver.findElement(By.tagName("h1"));
+        System.out.println(h1.getText());
+
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+
+
+}
